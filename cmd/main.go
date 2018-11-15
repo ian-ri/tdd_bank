a0ec02be494f36d55f5de37e4e9e000a9f784264
Mircea M <mircea.moise@gmail.com>
2018-11-15 11:37:43 +0000

    Refactored CheckBalance and Withdraw to use account service and introduced new AccountExists method<everyone>

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"os"
 	"strconv"
-	"github.com/mmircea16/tdd_bank/cmd/account"
 	"fmt"
 	"github.com/mmircea16/tdd_bank/cmd/account_service"
 )
@@ -24,7 +23,6 @@ func startBankUI(reader io.Reader, writer io.Writer) {
 	writer.Write([]byte("3. Check Balance\n"))
 	writer.Write([]byte("4. Withdraw Money\n"))
 
-	//myAccounts := make(map[string]*account.Account)
 
 	myAccountServices := account_service.NewAccountService()
 
@@ -40,8 +38,8 @@ func startBankUI(reader io.Reader, writer io.Writer) {
 			name := readFromCmdLine(scanner)
 			writer.Write([]byte("How much money?\n"))
 			amount := readIntFromCmdLine(writer, scanner)
-			//myAccount := account.NewAccount(name, amount)
-			err := myAccountServices.Open(name,amount)
+
+			err := myAccountServices.Open(name, amount)
 			if err != nil {
 				writer.Write([]byte("Cannot be negative\n")) //refactor error message TODO
 			} else {
@@ -54,7 +52,6 @@ func startBankUI(reader io.Reader, writer io.Writer) {
 
 		if input == "2" {
 
-
 			if myAccountServices.AnyAccountExists() {
 				writer.Write([]byte("Yes\n"))
 			} else {
@@ -63,38 +60,44 @@ func startBankUI(reader io.Reader, writer io.Writer) {
 		}
 
 		if input == "3" {
-			if account, found := isAccountExist(writer, scanner, myAccounts); found {
-				writer.Write([]byte(fmt.Sprintf("%d\n", account.CheckBalance())))
+
+			if !myAccountServices.AnyAccountExists() {
+				writer.Write([]byte("No account available\n"))
+			} else {
+				writer.Write([]byte("Enter account name\n"))
+				name := readFromCmdLine(scanner)
+				balance, err := myAccountServices.CheckBalance(name)
+				if err != nil {
+					writer.Write([]byte("account doesnt exist\n"))
+				} else {
+					writer.Write([]byte(fmt.Sprintf("%d\n", balance)))
+				}
 			}
 		}
 
 		if input == "4" {
-			if  account, found := isAccountExist(writer, scanner, myAccounts); found {
+			if !myAccountServices.AnyAccountExists() {
+				writer.Write([]byte("No account available\n"))
+			} else {
+				writer.Write([]byte("Enter account name\n"))
+				name := readFromCmdLine(scanner)
+				if !myAccountServices.AccountExists(name) {
+					writer.Write([]byte("account doesnt exist\n"))
+				}
+
 				writer.Write([]byte("How much money to withdraw?\n"))
 				amount := readIntFromCmdLine(writer, scanner)
-				account.Withdraw(amount)
-				writer.Write([]byte("Successful\n"))
+				err := myAccountServices.Withdraw(name, amount)
+				if err == nil {
+					writer.Write([]byte("Successful\n"))
+				}
 			}
+
 		}
 
 	}
 }
 
-func isAccountExist(writer io.Writer, scanner *bufio.Scanner, accounts map[string]*account.Account) (*account.Account, bool) {
-	if len(accounts) > 0 {
-		writer.Write([]byte("Enter account name\n"))
-		name := readFromCmdLine(scanner)
-
-		if account, found := accounts[name]; found {
-			return account, true
-		}else {
-			writer.Write([]byte("account doesnt exist\n"))
-		}
-	} else {
-		writer.Write([]byte("No account available\n"))
-	}
-	return nil, false
-}
 
 func readIntFromCmdLine(writer io.Writer, scanner *bufio.Scanner) int64 {
 	input := readFromCmdLine(scanner)
